4a548634c76cb35794c33f047a78495d2542583d
Aladdin Lee <siraj.n.lee@gmail.com>
2022-06-07 12:00:24 -0700

    Added a proper menu setup

--- source/introStates/MenuState.hx
+++ source/introStates/MenuState.hx
@@ -2,18 +2,25 @@ package introStates;
 
 import flixel.FlxG;
 import flixel.FlxState;
+import flixel.text.FlxText;
 import gameStates.PlayState;
 
 class MenuState extends FlxState
 {
 	override function create()
 	{
-		FlxG.switchState(new PlayState());
+		var t = new FlxText(0, 0);
+		t.text = "Rug Quest - Press Enter";
+		t.setFormat(null, 35);
+		t.screenCenter();
+		add(t);
 		super.create();
 	}
 
 	override function update(elapsed:Float)
 	{
+		if (FlxG.keys.justPressed.ENTER)
+			FlxG.switchState(new PlayState());
 		super.update(elapsed);
 	}
 }
