985350533358cb9a8db30289a64f43c3c8feb8da
Aladdin Lee <siraj.n.lee@gmail.com>
2022-06-07 13:17:59 -0700

    Adjusted slime hitbox

--- source/enemy/enemies/Slime.hx
+++ source/enemy/enemies/Slime.hx
@@ -8,6 +8,6 @@ class Slime extends Grounded
 	public function new(X:Int = 0, Y:Int = 0, ?Health:Int = 10, ?DMG:Int = 2)
 	{
 		super(X, Y, Health, DMG);
-		makeGraphic(32, 32, FlxColor.RED);
+		makeGraphic(16, 16, FlxColor.RED);
 	}
 }
